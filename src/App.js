adf14fc5c8c725e393972be72e9c60f3e99409aa
DIdaniel <73044605+DIdaniel@users.noreply.github.com>
2020-12-31 17:35:01 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 // import { Button, Progress } from 'semantic-ui-react';
 
 
+const customers = [
+  {
+   'id': 1,
+   'image': 'http://placeimg.com/64/64/any',
+   'name': '키아나',
+   'birthday': '950518',
+   'gender' : '여자',
+   'job' : '의적'
+  },
+  {
+    'id': 2,
+    'image': 'http://placeimg.com/64/64/2',
+    'name': '뚱이',
+    'birthday': '910609',
+    'gender' : '남자',
+    'job' : '포켓몬트레이너'
+   },
+   {
+    'id': 3,
+    'image': 'http://placeimg.com/64/64/3',
+    'name': '다니엘',
+    'birthday': '640529',
+    'gender' : '남자',
+    'job' : '프론트엔드 개발자'
+   }
+]
 
-function App() {
-  return (
-   <div className="gray-background">
-     <img src={ logo } lat="logo" />
-     <h2>Let's develop Management system!</h2>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </>
+     );
+  }
 }
 
 export default App;
