83156be9fc7c2b67ac071425c67b3b4c804d3c6a
DIdaniel <73044605+DIdaniel@users.noreply.github.com>
2021-01-01 10:22:58 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 // import { Button, Progress } from 'semantic-ui-react';
 
@@ -19,73 +20,78 @@ const styles = theme => ({
   },
   table: {
     minWidth: '100%'
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
-const customers = [
-  {
-   'id': 1,
-   'image': 'http://placeimg.com/64/64/1',
-   'name': '키아나',
-   'birthday': '950518',
-   'gender' : '여자',
-   'job' : '의적'
-  },
-  {
-    'id': 2,
-    'image': 'http://placeimg.com/32/32/any',
-    'name': '뚱이',
-    'birthday': '910609',
-    'gender' : '남자',
-    'job' : '포켓몬트레이너'
-   },
-   {
-    'id': 3,
-    'image': 'http://placeimg.com/64/64/3',
-    'name': '다니엘',
-    'birthday': '640529',
-    'gender' : '남자',
-    'job' : '프론트엔드 개발자'
-   }
-]
+/*
+
+1) constructor()
+2) compoenetWillMount()
+3) render()
+4) componentDidMount()
+
+
+state or props => shouldComponentUpdate()
+
+*/
 
 class App extends Component {
+
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  }
+
   render(){
 
     const { classes } = this.props;
 
     return (
       <Paper className={ classes.root }>
-      <Table className={ classes.table }>
-      <TableHead>
-        <TableRow>
-          <TableCell>번호</TableCell>
-          <TableCell>이미지</TableCell>
-          <TableCell>이름</TableCell>
-          <TableCell>생년월일</TableCell>
-          <TableCell>성별</TableCell>
-          <TableCell>직업</TableCell>
-        </TableRow>
-      </TableHead>
-       <TableBody>
-         {
-            customers.map(c => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              )
-            })
-          }
-       </TableBody>
-      </Table>
-        
+        <Table className={ classes.table }>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => {
+                  return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> )
+             }) :
+             <TableRow>
+               <TableCell colSpan='6' align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+               </TableCell>
+             </TableRow>
+            }
+          </TableBody>
+        </Table>
       </Paper>
      );
   }
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({message: "Hello Express man!"});
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+     'id': 1,
+     'image': 'https://placeimg.com/64/64/any',
+     'name': '키아나',
+     'birthday': '950518',
+     'gender' : '여자',
+     'job' : '의적'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/32/32/any',
+      'name': '뚱이',
+      'birthday': '910609',
+      'gender' : '남자',
+      'job' : '포켓몬트레이너'
+     },
+     {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/any',
+      'name': '다니엘',
+      'birthday': '640529',
+      'gender' : '남자',
+      'job' : '프론트엔드 개발자'
+     }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
